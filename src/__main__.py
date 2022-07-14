509e3bf94fd506d1a2e403562bf96e339a8fdc96
Adam Marton Beke <65732867+BekeAtGithub@users.noreply.github.com>
2022-07-14 11:39:48 -0400

    Update - open file fixed

--- src/__main__.py
+++ src/__main__.py
@@ -1,38 +1,42 @@
-from tkinter import *
-from tkinter import filedialog
+from tkinter import Button, Tk, filedialog, Text, END, WORD, INSERT
 
-root=Tk()
-root.geometry("500x600")
+def main():
+    root=Tk()
+    root.geometry("500x600")
+    main=root
 
-def save_file():
-    open_file=filedialog.asksaveasfile(mode='w',defaultextension=".txt")
-    if open_file is None:
-        return
-    text=str(entry.get(1.0,END))
-    open_file.write(text)
-    open_file.close()
+    def save_file():
+        open_file=filedialog.asksaveasfile(mode='w',defaultextension=".txt")
+        if open_file is None:
+            return
+        text=str(entry.get(1.0,END))
+        open_file.write(text)
+        open_file.close()
 
-def clear():
-    entry.delete(1.0,END)
+    def clear():
+        entry.delete(1.0,END)
 
-def open_file():
-    file=filedialog.askopenfile(mode='r',filetype=[{'text files','*.txt'}])
-    if file is not None: 
-        content=file.read()
-        print(content)
-    entry.insert(INSERT,content)
+    def open_file():
+        file=filedialog.askopenfile(mode='r',filetype=[('text files','*.txt')])
+        if file is not None: 
+            content=file.read()
+            print(content)
+        entry.insert(INSERT,content)
 
-b1=Button(root,text="save file",command=save_file)
-b1.place(x=10,y=10)
+    b1=Button(root,text="save file",command=save_file)
+    b1.place(x=10,y=10)
 
-b2=Button(root,text="clear",command=clear)
-b2.place(x=70,y=10)
+    b2=Button(root,text="clear",command=clear)
+    b2.place(x=70,y=10)
 
-b3=Button(root,text="open file",command=open_file)
-b3.place(x=110,y=10)
+    b3=Button(root,text="open file",command=open_file)
+    b3.place(x=110,y=10)
 
-entry=Text(root,height=33,width=58,wrap=WORD)
-entry.place(x=10,y=50)
+    entry=Text(root,height=33,width=58,wrap=WORD)
+    entry.place(x=10,y=50)
 
-root.mainloop()
-root.exitonclick()
+    root.mainloop()
+    root.exitonclick()
+
+if __name__ == "__main__":
+    main()
